1653ea7b61f14a3ecaa1a6391905de989a2bc42f
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-12-10 12:58:31 +0600

    comment added

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,4 +1,15 @@
+import { useForm } from "react-hook-form";
+
 const Card = () => {
+	const {
+		register,
+		handleSubmit,
+		// reset,
+	} = useForm();
+	const onSubmit = (data) => {
+		console.log(data);
+	};
+
 	return (
 		<div>
 			<div className="card card-compact w-96 bg-base-100 shadow-xl">
@@ -27,10 +38,25 @@ const Card = () => {
 						<div className="collapse bg-base-200">
 							<input type="checkbox" className="peer" />
 							<div className="collapse-title bg-info text-primary-content peer-checked:bg-info peer-checked:text-secondary-content">
-								Click me to show/hide content
+								Click to Comment
 							</div>
 							<div className="collapse-content bg-info text-primary-content peer-checked:bg-info peer-checked:text-secondary-content">
-								<p>hello</p>
+								<form className="card-body" onSubmit={handleSubmit(onSubmit)}>
+									<div className="form-control mt-6 mx-auto">
+										<label className="label">
+											<span className="label-text">Comment</span>
+										</label>
+										<input
+											type="text"
+											placeholder="Comment"
+											className="input input-bordered"
+											{...register("comment")}
+										/>
+									</div>
+									<button className="btn btn-success btn-outline ">
+										Comment
+									</button>
+								</form>
 							</div>
 						</div>
 					</div>
--- src/Pages/Media/Media.jsx
+++ src/Pages/Media/Media.jsx
@@ -1,5 +1,21 @@
+import Card from "../../Components/Card";
+import Title from "../../Components/Title";
+
 const Media = () => {
-	return <div>this is media=============</div>;
+	return (
+		<div>
+			<Title title={"Media Post"} icon={""} />
+			<div className="grid grid-cols-1 md:lg:xl:grid-cols-2 justify-items-center">
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+			</div>
+		</div>
+	);
 };
 
 export default Media;
