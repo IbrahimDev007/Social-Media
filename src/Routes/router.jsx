1327e9aff4c93fa02b5b46f6567a424694e1f502
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-12-12 19:24:30 +0600

    build modefy

--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -1,20 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import Media from "../Pages/Media/Media";
 import About from "../Pages/About/About";
-import Home from "../Pages/Home/home";
+// import Home from "../Pages/Home/home";
 import Layout from "../Layout/Layout";
-import Login from "../Pages/Login/login";
+// import Login from "../Pages/Login/login";
 import Register from "../Pages/Register/Register";
 import Details from "../Components/Details";
+import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
 //router added
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <Layout />,
-		loader: () => {
-			console.log("this is good");
-			return 1;
-		},
 		errorElement: <h1>This error</h1>,
 
 		children: [
