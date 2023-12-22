9a3b5a4c87aebf22dcb1fae230e6071516dff925
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-12-22 12:08:37 +0600

    framer motion title transation use at title

--- src/Components/Title.jsx
+++ src/Components/Title.jsx
@@ -1,11 +1,16 @@
-import "./Title.css";
+import { motion } from "framer-motion";
 const Title = ({ title, icon }) => {
 	return (
-		<div className=" alert ">
-			<span className="flex text-2xl text-amber-300 hover:text-green-600 gap-2  justify-center items-center">
-				{icon}
-				{title}
-			</span>
+		<div
+			role="alert"
+			className="alert alert-info flex justify-center items-center m-2 text-base-300"
+		>
+			<motion.div initial={{ x: -1000 }} animate={{ x: 0 }} exit={{ x: -1000 }}>
+				<span className="flex text-2xl text-amber-300 hover:text-green-600 gap-2 items-center">
+					{icon}
+					{title}
+				</span>
+			</motion.div>
 		</div>
 	);
 };
